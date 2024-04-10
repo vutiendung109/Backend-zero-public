d77df90839c1e71385eb2a1d55ba6cb9dd18292c
Vu Tien Dung <vudung10922@gmail.com>
2024-04-10 22:56:22 +0700

    #12

--- app.js
+++
@@ -1,1 +0,0 @@
-console.log('Vu Tien Dung')
---
+++ test.js
@@ -0,0 +1,15 @@
+//Load HTTP module (thư viện đã có sẵn khi cài đặt node.js)
+const http = require("http");
+const hostname = "127.0.0.1"; //cái này === http://localhost
+const port = 3300;
+//Create HTTP server and listen on port 3000 for requests
+const server = http.createServer((req, res) => {
+//Set the response HTTP header with HTTP status and Content type
+res.statusCode = 200;
+res.setHeader("Content-Type", "text/plain");
+res.end("Hello World\n Hi Dung");
+});
+//listen for request on port 3000, and as a callback function have the port listened on logged
+server.listen(port, hostname, () => {
+console.log(`Server running at http://${hostname}:${port}/`);
+});
